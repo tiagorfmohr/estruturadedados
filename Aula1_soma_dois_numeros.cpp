e34b3dc3813c19741079f5bc21c9f7a321cd8d79
tiagorfmohr <61715651+tiagorfmohr@users.noreply.github.com>
2020-08-04 21:03:48 -0300

    Add files via upload

---
+++ Aula1_soma_dois_numeros.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void)
+{
+    int a, b, soma;
+    printf("Digite um valor A: ");
+    scanf("%d",&a);
+    printf("Digite um valor B: ");
+    scanf("%d",&b);
+    soma = a + b;
+
+    printf("A soma e: %d", soma);
+
+
+
+}
